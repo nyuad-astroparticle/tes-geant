c764c8d1dcb8a498d5c77bd81158631ad3c1a271
Panos Oikonomou <panos.economou.v@gmail.com>
2023-05-29 12:57:37 +0400

    Set Dual MPI and analysis manager

---
+++ mpi_active.h
@@ -0,0 +1,3 @@
+#ifndef MPI
+#define MPI
+#endif
--- sim.cc
+++ sim.cc
@@ -5,28 +5,54 @@
 #include "ActionInitialization.hh"
 
 // importing mandatory g4 thingies
-#include "G4RunManagerFactory.hh"
 #include "G4UImanager.hh"
 #include "G4VisManager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
+
+#include "./mpi_active.h"
+
+#ifdef MPI
+#include "G4MPImanager.hh"
+#include "G4MPIsession.hh"
+#include "G4RunManager.hh"
+#else
+#include "G4RunManagerFactory.hh"
+#endif
+
+// If you are not in multithreaded mode assume you are in MPI mode
+
 int main(int argc,char**  argv){
 
 	// Evaluate the arguments
     G4UIExecutive* ui = nullptr;
-    if (argc == 1) {ui = new G4UIExecutive(argc,argv);}     // If no macro file has been created Create the UI
 
-	auto runManager = G4RunManagerFactory::CreateRunManager(); // creating a run manager
+#ifdef MPI 
+	G4MPImanager* mpiManager    = new G4MPImanager(argc,argv);
+    mpiManager->SetVerbose(1);
+
+    G4MPIsession* session       = mpiManager->GetMPIsession();
+    G4String prompt             = "[40;01;33mG4MPI[40;31m(%s)[40;36m[%/][00;30m:";
+    session-> SetPrompt(prompt);
+
+    G4RunManager* runManager    = new G4RunManager();
+#else
+    if (argc == 1) {ui = new G4UIExecutive(argc,argv);}     // If no macro file has been created Create the UI
+    auto runManager = G4RunManagerFactory::CreateRunManager(); // creating a run manager
+#endif
 	runManager->SetUserInitialization(new MyDetectorConstruction);
 	runManager->SetUserInitialization(new MyPhysicsList);
 	runManager->SetUserInitialization(new MyActionInitialization);
 
 	runManager->Initialize();
 
-	G4UImanager* uiManager = G4UImanager::GetUIpointer(); // get the pointer of the ui manager duh
-
-	G4VisManager *visManager = new G4VisExecutive();
+    G4VisManager *visManager = new G4VisExecutive();
 	visManager->Initialize();
+
+#ifdef MPI
+    session->SessionStart();
+#else
+	G4UImanager* uiManager = G4UImanager::GetUIpointer(); // get the pointer of the ui manager duh
 
 	// Process the macro file or strat GUI
     if (!ui){
@@ -53,9 +79,13 @@ int main(int argc,char**  argv){
         ui->SessionStart();
         delete ui;
     }
+#endif
 
 	// deleting the run manager
 	delete visManager;
+#ifdef MPI
+    delete mpiManager;
+#endif
 	delete runManager;
 	return 0;
 }
