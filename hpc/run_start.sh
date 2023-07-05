9a67e2341f628d2a9e37d33a4e46aff5873818f4
Panos Oikonomou <po524@nyu.edu>
2023-07-05 20:38:23 +0400

    added timing info

--- hpc/run_start.sh
+++ hpc/run_start.sh
@@ -18,7 +18,10 @@ source load_modules.sh
 
 # Run the application
 cd /scratch/po524/tes-geant/build/
+start_time=`date +%s`
 mpiexec -n 1024 ./sim ./macros/start.mac
+end_time=`date +%s`
+echo execution time was `expr $end_time - $start_time` s.
 echo "SIM IS DONE!"
 echo "MERGING!"
 ../hpc/merge.sh ./output > ./output/merge.csv
