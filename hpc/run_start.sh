b1d117a65cc88bcfe0246e1075fa5efe3fb7177d
Panos Oikonomou <po524@nyu.edu>
2023-07-10 15:35:38 +0400

    More cores

--- hpc/run_start.sh
+++ hpc/run_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Number of tasks 
-#SBATCH --ntasks 1024
+#SBATCH --ntasks 3024
 # Set WALLTIME
-#SBATCH --time 00:30:00
+#SBATCH --time 05:00:00
 # Set output and error files
 #SBATCH -o start-job.%J.out
 #SBATCH -e start-job.%J.err
@@ -19,9 +19,9 @@ source load_modules.sh
 # Run the application
 cd /scratch/po524/tes-geant/build/
 start_time=`date +%s`
-mpiexec -n 1024 ./sim ./macros/start.mac
+mpiexec -n 3024 ./sim ./macros/start.mac
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
 echo "SIM IS DONE!"
-echo "MERGING!"
-../hpc/merge.sh ./output > ./output/merge.csv
+# echo "MERGING!"
+# ../hpc/merge.sh ./output > ./output/merge.csv
