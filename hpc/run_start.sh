a427c7bcaf5c16c36a45909b5bd4d2b4f9701411
Panos Oikonomou <po524@nyu.edu>
2023-07-26 08:41:50 +0400

    More memory per task

--- hpc/run_start.sh
+++ hpc/run_start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Number of tasks 
-#SBATCH --ntasks 3024
+#SBATCH --ntasks 1024 
+# Set Memory
+#SBATCH --mem 64GB
 # Set WALLTIME
 #SBATCH --time 05:00:00
 # Set output and error files
 #SBATCH -o start-job.%J.out
 #SBATCH -e start-job.%J.err
-
+# Send email when done
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=po524@nyu.edu
 
 #----------------------------------#
 #------------EXECUTION-------------#
@@ -19,7 +23,7 @@ source load_modules.sh
 # Run the application
 cd /scratch/po524/tes-geant/build/
 start_time=`date +%s`
-mpiexec -n 3024 ./sim ./macros/start.mac
+mpiexec -n 1024 ./sim ./macros/start.mac
 end_time=`date +%s`
 echo execution time was `expr $end_time - $start_time` s.
 echo "SIM IS DONE!"
