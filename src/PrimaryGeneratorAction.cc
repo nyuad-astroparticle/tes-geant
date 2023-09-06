ab2372ad7e3fa9dc2105bcc367a5b69787d187b3
F7East <tengiz.ibrayev@gmail.com>
2023-09-06 12:38:47 +0400

    saber + th

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -117,7 +117,7 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent)
 	// Position and confine within the copper box
 	particleGun->GetCurrentSource()->GetPosDist()->SetPosDisType("Surface");
 	particleGun->GetCurrentSource()->GetPosDist()->SetPosDisShape("Para");
-	particleGun->GetCurrentSource()->GetPosDist()->SetCentreCoords(G4ThreeVector( -35.2 * cm / 2 - 1.*cm, -106. * cm / 2 + 11.5*cm, 0));
+	particleGun->GetCurrentSource()->GetPosDist()->SetCentreCoords(G4ThreeVector( -35.2 * cm / 2 - 2*mm - 1*cm/2 - 0.5*cm/2 -2 * mm, -698.771 * mm + 11.5 * cm, 0));
 	particleGun->GetCurrentSource()->GetPosDist()->SetHalfX(0.25 * cm);
 	particleGun->GetCurrentSource()->GetPosDist()->SetHalfY(1.5 * cm);
 	particleGun->GetCurrentSource()->GetPosDist()->SetHalfZ(1.5 * cm);
